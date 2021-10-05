84673013e620d50b7246e9ba05c90a22441ff1a1
SantiCHP <santichadmin@gmail.com>
2021-10-05 21:52:52 +0100

    comparative
    configuring charts

--- my_chart.js
+++ my_chart.js
@@ -24,8 +24,26 @@ var myChart = new Chart(earning, {
     data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         datasets: [{
-            label: 'Profits 2020 in €',
+            label: 'Profits 2019 in €',
             data: [1200, 980, 1150, 1500, 1730, 2100, 1600, 1320, 1400, 1875, 2300, 2500],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)',
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+            ]
+        },
+        {
+            label: 'Profits 2020 in €',
+            data: [1800, 1480, 1850, 2300, 2130, 2600, 3100, 2420, 2800, 3775, 2600, 3300],
             backgroundColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
